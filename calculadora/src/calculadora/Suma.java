745db2cba75de0eb3dad8605c93fd86b0ec3c172
ProElim <elimorellanae@gmail.com>
2021-04-15 20:46:35 -0400

    2 Se agrego codigo java en paquete Calculadora

---
+++ calculadora/src/calculadora/Suma.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class Suma {
+	private int a=0;
+	private int b=0;
+	
+	public Suma(int a, int b) {
+		this.a=a;
+		this.b=b;
+	}
+	public int sumar() {
+		
+		return a+b;
+	}
+			
+}
