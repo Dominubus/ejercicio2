cb262a930866e81aec93133c2bbba82cc347fcf8
ProElim <elimorellanae@gmail.com>
2021-04-15 21:11:46 -0400

    describir lo que se modificará

---
+++ aritmetica/Resta.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+public class Resta {
+	private int a=0;
+	private int b=0;
+	
+	public Resta(int a, int b) {
+		this.a=a;
+		this.b=b;
+	}
+	public int restar() {
+		
+		return a-b;
+	}
+
+}
